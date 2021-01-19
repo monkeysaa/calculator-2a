56c801f95f9251340d7bbdca686f179b3b2af08e
Alison Cordeiro <acordeiro@gmail.com>
2021-01-19 00:32:59 +0000

    Added reduce feature - needs cube and square

--- arithmetic.py
+++ arithmetic.py
@@ -3,13 +3,13 @@
 
 def add(num1, num2):
     """Return the sum of the two input integers."""
-    add_num = num1 + num2
+    
     return num1 + num2
 
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
-    sub_num = num1 - num2
+    
     return num1 - num2
 
 
--- calculator.py
+++ calculator.py
@@ -3,13 +3,14 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, add_mult, add_cubes)
 
+from functools import reduce
 
 # Replace this with your code
 """
 while exit_condition_not_reached:
     input = consume_input()
     *option: error-handle input*
-    output = evaluate_input(input)
+    output = evaluate_input(input) 
     print(output)
 
 repeat forever:
@@ -31,39 +32,58 @@ while (user_input != 'q'):
     user_input = input('Enter your equation > ')  # > + 2 1
     
     tokens = user_input.split(' ')      # tokens = ['+', '2', '1']
-    operation = tokens[0]
+    operation = tokens.pop(0)
+    values = []
+    for item in tokens: 
+        values.append(float(item))
 
-    if len(tokens) == 3:
-        num2 = float(tokens[2])
-        num1 = float(tokens[1])
-    if len(tokens) == 2:
-        num1 = float(tokens[1])        
+    
+    
+    """
+        if len(tokens) > 3:
 
+            
+            # pop off the symbol at token[0] and then turn the rest into floats
+            # pass that into reduce
+
+
+        if len(tokens) == 3:
+            num2 = float(tokens[2])
+            num1 = float(tokens[1])
+        if len(tokens) == 2:
+            num1 = float(tokens[1])        
+    """
     if operation == 'q':
         print('Exiting calculator.')
         break
 
+    if operation == 'square' or operation == 'cube':
+        print('This feature needs to be implemented.')
+        break
+
     elif operation == '+': 
-        print(add(num1, num2))
+        math_function = add
     
     elif operation == '-':
-        print(subtract(num1, num2))
+        math_function = subtract
 
     elif operation == '*':
-        print(multiply(num1, num2))
+        math_function = multiply
         
     elif operation == 'square':
-        print(square(num1))
+        math_function = square
     
     elif operation == 'cube':
-        print(cube(num1))
+        math_function = cube
     
     elif operation == 'pow':
-        print(power(num1, num2))
+        math_function = power
     
     elif operation == 'mod':
-        print(mod(num1, num2))
+        math_function = mod
     
     elif operation == '/':
-        print(divide(num1, num2))
+        math_function = divide
+    
+    print(reduce(math_function, values))
     
